bd78d4421e0a14b4c9f1a176eb89f8e759399747
Michelle Rodriguez <117132427+michytrv@users.noreply.github.com>
2022-12-14 16:02:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+// This is the array that will hold the todo list items
+let todoItems = [];
+
+
+function renderTodo(todo) {
+    const list = document.querySelector('.js-todo-list');
+    const item = document.querySelector(`[data-key='${todo.id}']`);
+  
+    // add this if block
+    if (todo.deleted) {
+        item.remove();
+        // add this line to clear whitespace from the list container
+        // when `todoItems` is empty
+        if (todoItems.length === 0) list.innerHTML = '';
+        return
+      }
+  
+    const isChecked = todo.checked ? 'done': '';
+    const node = document.createElement("li");
+    node.setAttribute('class', `todo-item ${isChecked}`);
+    node.setAttribute('data-key', todo.id);
+    node.innerHTML = `
+      <input id="${todo.id}" type="checkbox"/>
+      <label for="${todo.id}" class="tick js-tick"></label>
+      <span>${todo.text}</span>
+      <button class="delete-todo js-delete-todo">
+      <svg><use href="#delete-icon"></use></svg>
+      </button>
+    `;
+  
+    if (item) {
+      list.replaceChild(node, item);
+    } else {
+      list.append(node);
+    }
+  }
+
+
+// This function will create a new todo object based on the
+// text that was entered in the text input, and push it into
+// the `todoItems` array
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+  renderTodo(todo);
+  console.log(todoItems);
+}
+
+// Select the form element
+const form = document.querySelector('.js-form');
+// Add a submit event listener
+form.addEventListener('submit', event => {
+  // prevent page refresh on form submission
+  event.preventDefault();
+  // select the text input
+  const input = document.querySelector('.js-todo-input');
+
+  // Get the value of the input and remove whitespace
+  const text = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+});
+
+
+// Select the entire list
+const list = document.querySelector('.js-todo-list');
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('js-tick')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+
+  // add this `if` block
+  if (event.target.classList.contains('js-delete-todo')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+});
+
+function toggleDone(key) {
+    // findIndex is an array method that returns the position of an element
+    // in the array.
+    const index = todoItems.findIndex(item => item.id === Number(key));
+    // Locate the todo item in the todoItems array and set its checked
+    // property to the opposite. That means, `true` will become `false` and vice
+    // versa.
+    todoItems[index].checked = !todoItems[index].checked;
+    renderTodo(todoItems[index]);
+  }
+
+
+  function deleteTodo(key) {
+    // find the corresponding todo object in the todoItems array
+    const index = todoItems.findIndex(item => item.id === Number(key));
+    // Create a new object with properties of the current todo item
+    // and a `deleted` property which is set to true
+    const todo = {
+      deleted: true,
+      ...todoItems[index]
+    };
+    // remove the todo item from the array by filtering it out
+    todoItems = todoItems.filter(item => item.id !== Number(key));
+    renderTodo(todo);
+  }
+
